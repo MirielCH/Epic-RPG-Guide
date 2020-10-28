254463ceb948c2c911eca3ec030d6a446bcc14db
kklsid <wernerenz@zoneseven.ch>
2020-10-28 22:43:45 +0100

    Added commands "settings" and "setprogress"

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import discord
 import sqlite3
 import shutil
+import asyncio
 
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -31,14 +32,14 @@ bot = discord.Client()
 # Check database for stored prefix, if none is found, a record is inserted and the default prefix $ is used, return all bot prefixes
 def get_prefix_all(bot, message):
     cur=erg_db.cursor()
-    cur.execute('SELECT * FROM global_settings where guild_id=?', (message.guild.id,))
+    cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
     
     if record:
         prefix = record[1]
     else:
         try:
-            cur.execute('INSERT INTO global_settings VALUES (?, ?)', (message.guild.id, default_prefix,))
+            cur.execute('INSERT INTO settings_guild VALUES (?, ?)', (message.guild.id, default_prefix,))
         except sqlite3.Error as error:
             print(f'Error inserting into database.\n{error}')
 
@@ -49,7 +50,7 @@ def get_prefix_all(bot, message):
 # Check database for stored prefix, if none is found, the default prefix $ is used, return only the prefix
 def get_prefix(bot, message):
     cur=erg_db.cursor()
-    cur.execute('SELECT * FROM global_settings where guild_id=?', (message.guild.id,))
+    cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
     
     if record:
@@ -62,22 +63,55 @@ def get_prefix(bot, message):
 # Set new prefix
 def set_prefix(bot, message, new_prefix):
     cur=erg_db.cursor()
-    cur.execute('SELECT * FROM global_settings where guild_id=?', (message.guild.id,))
+    cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
     
     if record:
         try:
-            cur.execute('UPDATE global_settings SET prefix = ? where guild_id = ?', (new_prefix, message.guild.id,))
+            cur.execute('UPDATE settings_guild SET prefix = ? where guild_id = ?', (new_prefix, message.guild.id,))
         except sqlite3.Error as error:
             print(f'Error updating record in database.\n{error}')           
     else:
         try:
-            cur.execute('INSERT INTO global_settings VALUES (?, ?)', (message.guild.id, new_prefix,))
+            cur.execute('INSERT INTO settings_guild VALUES (?, ?)', (message.guild.id, new_prefix,))
+        except sqlite3.Error as error:
+            print(f'Error inserting into database.\n{error}')
+      
+bot = commands.Bot(command_prefix=get_prefix_all)
+
+# Check database for stored progress settings, if none is found, the default settings TT0 and not ascended are saved and used, return both
+def get_settings(bot, message):
+    cur=erg_db.cursor()
+    cur.execute('SELECT * FROM settings_user where user_id=?', (message.author.id,))
+    record = cur.fetchone()
+    
+    if record:
+        current_settings = (record[1], record[2])
+    else:
+        try:
+            cur.execute('INSERT INTO settings_user VALUES (?, ?, ?)', (message.author.id, '0', 'false',))
+            current_settings = get_settings(bot, message)
         except sqlite3.Error as error:
             print(f'Error inserting into database.\n{error}')
         
+    return current_settings
 
-bot = commands.Bot(command_prefix=get_prefix_all)
+# Set progress settings
+def set_progress(bot, message, new_tt, new_ascended):
+    cur=erg_db.cursor()
+    cur.execute('SELECT * FROM settings_user where user_id=?', (message.author.id,))
+    record = cur.fetchone()
+    
+    if record:
+        try:
+            cur.execute('UPDATE settings_user SET timetravel = ?, ascended = ? where user_id = ?', (new_tt, new_ascended, message.author.id,))
+        except sqlite3.Error as error:
+            print(f'Error updating record in database.\n{error}')           
+    else:
+        try:
+            cur.execute('INSERT INTO settings_user VALUES (?, ?, ?)', (message.author.id, new_tt, new_ascended,))
+        except sqlite3.Error as error:
+            print(f'Error inserting into database.\n{error}')
 
 @bot.event
 async def on_ready():
@@ -88,7 +122,7 @@ async def on_ready():
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
         return
-    elif isinstance(error, (commands.MissingPermissions)):
+    if isinstance(error, (commands.MissingPermissions)):
         await ctx.send(f'Sorry, you are not allowed to use this command.')
 
 # Command "setprefix" - Sets new prefix (if user has "manage server" permission)
@@ -110,5 +144,68 @@ async def setprefix(ctx, *new_prefix):
 async def prefix(ctx, *args):
     current_prefix = get_prefix(bot, ctx)
     await ctx.send(f'The prefix for this server is `{current_prefix}`\nTo change the prefix use `{current_prefix}setprefix [prefix]`')
+
+# Command "settings" - Returns current user progress settings
+@bot.command()
+async def settings(ctx, *args):
+    current_settings = get_settings(bot, ctx)
+    if current_settings[1] == 'true':
+        current_ascension = 'ascended'
+    else:
+        current_ascension = 'not ascended'
+        
+    await ctx.send(f'You are currently set as TT{current_settings[0]}, {current_ascension}')
     
+# Command "setprogress" - Sets TT and ascension
+@bot.command()
+async def setprogress(ctx, *args):
+    
+    def check(m):
+        return m.author == ctx.author
+    
+    try:
+        await ctx.send('What TT are you currently in? `[0-999]`')
+        answer_tt = await bot.wait_for('message', check=check, timeout = 30)
+        try:            
+            if 0 <= int(answer_tt.content) <= 999:
+                new_tt = int(answer_tt.content)
+                await ctx.send('Are you ascended? `[yes/no]`')
+                answer_ascended = await bot.wait_for('message', check=check, timeout=30)
+            
+                if answer_ascended.content.lower() in ['yes','y']:
+                    new_ascended = 'true'         
+                    set_progress(bot, ctx, new_tt, new_ascended)  
+                    current_settings = get_settings(bot, ctx)
+                    
+                    if current_settings[1] == 'true':
+                        current_ascension = 'ascended'
+                    else:
+                        current_ascension = 'not ascended'
+                    
+                    await ctx.send(f'Settings changed, you are now set as TT{current_settings[0]}, {current_ascension}.')     
+                
+                elif answer_ascended.content.lower() in ['no','n']:
+                    new_ascended = 'false'
+                    set_progress(bot, ctx, new_tt, new_ascended)        
+                    current_settings = get_settings(bot, ctx)
+                
+                    if current_settings[1] == 'true':
+                        current_ascension = 'ascended'
+                    else:
+                        current_ascension = 'not ascended'
+                    
+                    await ctx.send(f'Settings changed, you are now set as TT{current_settings[0]}, {current_ascension}.')     
+                
+                else:
+                    await ctx.send('Please answer with `yes` or `no`. Aborting.')
+            
+            else:
+                await ctx.send('Please enter a number from 0 to 999. Aborting.')
+        
+        except:
+            await ctx.send('Please answer with a valid number. Aborting.')
+    
+    except asyncio.TimeoutError as error:
+        await ctx.send('You took too long to answer. Aborting.')
+
 bot.run(TOKEN)
