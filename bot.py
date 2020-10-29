999733631ab353bdc2f80ced72d67f51dfd7f7c5
kklsid <wernerenz@zoneseven.ch>
2020-10-29 19:51:57 +0100

    Added first time recognition and cleaned up a little.

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ erg_db = sqlite3.connect(dbfile, isolation_level=None)
 bot = discord.Client()
 
 # Check database for stored prefix, if none is found, a record is inserted and the default prefix $ is used, return all bot prefixes
-def get_prefix_all(bot, message):
+async def get_prefix_all(bot, message):
     cur=erg_db.cursor()
     cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
@@ -45,10 +45,10 @@ def get_prefix_all(bot, message):
 
         prefix = default_prefix
         
-    return commands.when_mentioned_or(*prefix)(bot, message)
+    return commands.when_mentioned_or(* prefix)(bot, message)
 
 # Check database for stored prefix, if none is found, the default prefix $ is used, return only the prefix
-def get_prefix(bot, message):
+async def get_prefix(bot, message):
     cur=erg_db.cursor()
     cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
@@ -61,7 +61,7 @@ def get_prefix(bot, message):
     return prefix
 
 # Set new prefix
-def set_prefix(bot, message, new_prefix):
+async def set_prefix(bot, message, new_prefix):
     cur=erg_db.cursor()
     cur.execute('SELECT * FROM settings_guild where guild_id=?', (message.guild.id,))
     record = cur.fetchone()
@@ -80,7 +80,7 @@ def set_prefix(bot, message, new_prefix):
 bot = commands.Bot(command_prefix=get_prefix_all)
 
 # Check database for stored progress settings, if none is found, the default settings TT0 and not ascended are saved and used, return both
-def get_settings(bot, message):
+async def get_settings(bot, message):
     cur=erg_db.cursor()
     cur.execute('SELECT * FROM settings_user where user_id=?', (message.author.id,))
     record = cur.fetchone()
@@ -89,15 +89,23 @@ def get_settings(bot, message):
         current_settings = (record[1], record[2])
     else:
         try:
-            cur.execute('INSERT INTO settings_user VALUES (?, ?, ?)', (message.author.id, '0', 'false',))
-            current_settings = get_settings(bot, message)
+            cur.execute('INSERT INTO settings_user VALUES (?, ?, ?)', (message.author.id, '0', 'not ascended',))
+            #current_settings = await get_settings(bot, message)
+            await first_time_user(bot, message)
+            return
         except sqlite3.Error as error:
             print(f'Error inserting into database.\n{error}')    
   
     return current_settings
 
+async def first_time_user(bot, message):
+    current_settings = await get_settings(bot, message)
+    await message.send(f'Hey there, **{message.author.name}**, looks like we haven\'t met before.\nI have set your progress to '\
+                f'**TT{current_settings[0]}**, **{current_settings[1]}**.\n'\
+                f'If I guessed wrong, please use `{await get_prefix(bot, message)}setprogress` to change your settings.')
+
 # Set progress settings
-def set_progress(bot, message, new_tt, new_ascended):
+async def set_progress(bot, message, new_tt, new_ascended):
     cur=erg_db.cursor()
     cur.execute('SELECT * FROM settings_user where user_id=?', (message.author.id,))
     record = cur.fetchone()
@@ -123,37 +131,34 @@ async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
         return
     elif isinstance(error, (commands.MissingPermissions)):
-        await ctx.send(f'Sorry, you are not allowed to use this command.')
+        await ctx.send(f'Sorry **{ctx.author.name}**, you are not allowed to use this command.')
 
 # Command "setprefix" - Sets new prefix (if user has "manage server" permission)
 @bot.command()
 @commands.has_permissions(manage_guild=True)
 async def setprefix(ctx, *new_prefix):
-    current_prefix = get_prefix(bot, ctx)
+    current_prefix = await get_prefix(bot, ctx)
     if new_prefix:
         if len(new_prefix)>1:
             await ctx.send(f'Too many arguments.\nCommand syntax: `{current_prefix}setprefix [prefix]`')
         else:
-            set_prefix(bot, ctx, new_prefix[0])
-            await ctx.send(f'Prefix changed to `{get_prefix(bot, ctx)}`')
+            await set_prefix(bot, ctx, new_prefix[0])
+            await ctx.send(f'Prefix changed to `{await get_prefix(bot, ctx)}`')
     else:
         await ctx.send(f'Command syntax: `{current_prefix}setprefix [prefix]`')
 
 # Command "prefix" - Returns current prefix
 @bot.command()
 async def prefix(ctx, *args):
-    current_prefix = get_prefix(bot, ctx)
+    current_prefix = await get_prefix(bot, ctx)
     await ctx.send(f'The prefix for this server is `{current_prefix}`\nTo change the prefix use `{current_prefix}setprefix [prefix]`')
 
 # Command "settings" - Returns current user progress settings
 @bot.command()
 async def settings(ctx, *args):
-    current_settings = get_settings(bot, ctx)
-    if current_settings[1] == 'true':
-        current_ascension = 'ascended'
-    else:
-        current_ascension = 'not ascended'
-    await ctx.send(f'You are currently set as TT{current_settings[0]}, {current_ascension}.')
+    current_settings = await get_settings(bot, ctx)
+    await ctx.send(f'**{ctx.author.name}**, your progress is currently set to **TT{current_settings[0]}**, **{current_settings[1]}**.\n'\
+        f'Use `{await get_prefix(bot, ctx)}setprogress` if you want to change your settings.')
     
 # Command "setprogress" - Sets TT and ascension
 @bot.command()
@@ -163,38 +168,32 @@ async def setprogress(ctx, *args):
         return m.author == ctx.author
     
     try:
-        await ctx.send('What TT are you currently in? `[0-999]`')
+        await ctx.send(f'**{ctx.author.name}**, what TT are you currently in? `[0-999]`')
         answer_tt = await bot.wait_for('message', check=check, timeout = 30)
         try:            
             if 0 <= int(answer_tt.content) <= 999:
                 new_tt = int(answer_tt.content)
-                await ctx.send('Are you ascended? `[yes/no]`')
+                await ctx.send(f'**{ctx.author.name}**, are you ascended? `[yes/no]`')
                 answer_ascended = await bot.wait_for('message', check=check, timeout=30)
                 if answer_ascended.content.lower() in ['yes','y']:
-                    new_ascended = 'true'         
-                    set_progress(bot, ctx, new_tt, new_ascended)  
-                    current_settings = get_settings(bot, ctx)
-                    if current_settings[1] == 'true':
-                        current_ascension = 'ascended'
-                    else:
-                        current_ascension = 'not ascended'
-                    await ctx.send(f'Settings changed, you are now set as TT{current_settings[0]}, {current_ascension}.')     
+                    new_ascended = 'ascended'         
+                    await set_progress(bot, ctx, new_tt, new_ascended)  
+                    current_settings = await get_settings(bot, ctx)
+                    await ctx.send(f'Alright **{ctx.author.name}**, your progress is now set to **TT{current_settings[0]}**, **{current_settings[1]}**.')     
                 elif answer_ascended.content.lower() in ['no','n']:
-                    new_ascended = 'false'
-                    set_progress(bot, ctx, new_tt, new_ascended)        
-                    current_settings = get_settings(bot, ctx)
-                    if current_settings[1] == 'true':
-                        current_ascension = 'ascended'
-                    else:
-                        current_ascension = 'not ascended'
-                    await ctx.send(f'Settings changed, you are now set as TT{current_settings[0]}, {current_ascension}.')     
+                    new_ascended = 'not ascended'
+                    await set_progress(bot, ctx, new_tt, new_ascended)        
+                    current_settings = await get_settings(bot, ctx)
+                    await ctx.send(f'Alright **{ctx.author.name}**, your progress is now set to **TT{current_settings[0]}**, **{current_settings[1]}**.')     
                 else:
-                    await ctx.send('Please answer with `yes` or `no`. Aborting.')
+                    await ctx.send(f'**{ctx.author.name}**, please answer with `yes` or `no`. Aborting.')
             else:
-                await ctx.send('Please enter a number from 0 to 999. Aborting.')
+                await ctx.send(f'**{ctx.author.name}**, please enter a number from 0 to 999. Aborting.')
         except:
-            await ctx.send('Please answer with a valid number. Aborting.')  
+            await ctx.send(f'**{ctx.author.name}**, please answer with a valid number. Aborting.')  
     except asyncio.TimeoutError as error:
-        await ctx.send('You took too long to answer. Aborting.')
+        await ctx.send(f'**{ctx.author.name}**, you took too long to answer. Aborting.')
+
+# Aliases: @commands.command(aliases=['testcommand', 'testing'])
 
 bot.run(TOKEN)
